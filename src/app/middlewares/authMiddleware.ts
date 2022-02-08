94a8e396fcef7a74fafe37f9c4a199b0fb25f883
Myllena Cruz <myllena.cruz@tcsindustrial.com.br>
2022-02-08 19:24:41 -0300

    Middlewares de autenticação

---
+++ src/app/middlewares/authMiddleware.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface TokenPayload {
+	id: string;
+	iat: number;
+	exp: number;
+}
+
+export default function authMiddlewares(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	const { authorization } = req.headers;
+
+	if (!authorization) {
+		return res.status(401).json({ error: "Not authorized!" });
+	}
+
+	const token = authorization.replace("Bearer", "").trim();
+
+	try {
+		const data = jwt.verify(token, "secret");
+
+		const { id } = data as TokenPayload;
+
+		req.userId = id;
+
+		return next;
+	} catch {
+		return res.status(401);
+	}
+}
