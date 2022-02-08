78424b5aeb62d37ee66c402dc43b970747594bdf
Myllena Cruz <myllena.cruz@tcsindustrial.com.br>
2022-02-07 21:25:48 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import routes from "./routes";
+
+const app = express();
+app.use(express.json());
+
+app.use(routes);
+
+app.set("port", (3000));
+
+app.listen(app.get("port"), function () {
+	console.log("Server started at port", app.get("port"));
+});
