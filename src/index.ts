e561fb662cf3b2d976fa66768e98b7ffb1e3726a
Myllena Cruz <myllena.cruz@tcsindustrial.com.br>
2022-02-08 17:37:20 -0300

    Instalação TypeORM

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import "reflect-metadata";
+
 import express from "express";
 import routes from "./routes";
 
@@ -6,7 +8,7 @@ app.use(express.json());
 
 app.use(routes);
 
-app.set("port", (3000));
+app.set("port", 3000);
 
 app.listen(app.get("port"), function () {
 	console.log("Server started at port", app.get("port"));
